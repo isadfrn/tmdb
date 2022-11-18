4bbbb69fcb78b99b0fe82dcbbff311ab9e645f3b
isadfrn <isadfrn@gmail.com>
2022-11-17 23:52:41 -0300

    refactor: changing all codebase structure

--- client/src/components/CardGrid/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container, Title, Grid } from './style';
-export function CardGrid({ title, children }) {
-  return (
-    <Container>
-      <Title>{title}</Title>
-      <Grid>{children}</Grid>
-    </Container>
-  );
-}
--- client/src/components/Navbar/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div``;
-
-export const Nav = styled.nav`
-  padding: 1rem 2rem;
-  background-color: ${({ theme }) => theme.COLORS.BLUE_900};
-`;
